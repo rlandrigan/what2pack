6da2de935eb150031acf39509fd5af4c62cd2115
rlandrigan <ral@eclectek.com>
2025-03-28 19:10:15 -0500

    Update creds.php

--- planner/inc/creds.php
+++ planner/inc/creds.php
@@ -1,8 +1,8 @@
 <?php 
-$DB_host = 'mysql.scouts13.org';
-$DB_user = 'scouts13org';
-$DB_pass = 'SN^TcTAx';
-$DB_name = 'scouts13_org';
+$DB_host = 'xxx';
+$DB_user = 'xxx';
+$DB_pass = 'xxx';
+$DB_name = 'xxx';
 
-$db = mysqli_connect('mysql.scouts13.org', 'scouts13org', 'SN^TcTAx', 'scouts13_org');
+	 $db = mysqli_connect('xxx', 'xxx', 'xxx', 'xxx');
 ?>
